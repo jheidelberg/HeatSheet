f82420e522e55e43db722dc8a890049fb46d1c55
johnh <johnatjencast@hotmail.com>
2013-11-27 11:53:02 -0600

    Took the report.js to simplify for multiple reports and added code to check what report and load and parse accordingly.

--- HeatSheet/CodeBehind/report.js
+++ HeatSheet/CodeBehind/report.js
@@ -5,93 +5,76 @@ var utils = require('util');
 var SelStmt = "Select * ";
 var FromStmt = "From ";
 var WhereStmt = "Where ";
+var db = new sqlite3.Database('HeatSheet.sql3');
 
 function rpt(request, response, fullBody) {
 
     
 	var ary = new Array();
 	ary = querystring.parse(fullBody);
-    /*
-	for (var x in ary) {
-		//decodedBody += x + " x: " + ary[x] + "<br>";
-		switch (x){
-			case "Table" :
-				FromStmt = "From " + ary[x] + " ( ";
-				break;
-            default:
-                WhereStmt += x + " = '" + ary[x] + "' and "
-                break;
-
-		} // end switch
-						
-	} //end var x in array
-    */
-
-    // request ended -> do something with the data
-    
-    response.writeHead(200, "OK", { 'Content-Type': 'application/json' });
     var ResponseText = ""; // = querystring.parse(fullBody);
-	var db = new sqlite3.Database('HeatSheet.sql3');
-	var qry = fs.readFileSync('./HeatSheet/CodeBehind/CertQry.sql');
-    
-    // two ways of running the sql: db.serialize that runs inline and 
-    //  will block execution while it is running, or regular and will 
-    //  require a callback that runs the following exec.
-
-//Multiple-statement queries are supported; each statement's result set is retuned to the callback as a separate parameter:
-
-//var q = db.query("UPDATE bar SET z=20; SELECT SUM(z) FROM bar;",
-//                 function (update, select) {
-//                   assert(update.rowsAffected == 1);
-//                   assert(select[0]['SUM(z)'] == 20);
-//                 });
-//
-//An array of all result sets is available as the .all property on each result set:
-//
-//assert(q.all[1].length == 1);
-    var MyJSONObj = new Array();
-    function Sanitize(str){
-        if (str) { str = str.replace("'", "''"); }else{str='';};
-        return str;
-    }
-    db.serialize(function () {
-
-        qry = qry.toString().replace('@HEAT', Sanitize(ary["heat"]));
-        qry = qry.toString().replace('@PART', Sanitize(ary["part"]));
-
-        db.all(qry, function (err, rows) {
-            if (err) {
-                MyJSONObj[0] = { 'test': err, 'status': 'error', 'message': 'Error: ' + err };
-                response.write(JSON.stringify(MyJSONObj));
-                response.end;
-                return;
-            }
-            if (rows) {
-                if (rows.length == 0) {
-                    MyJSONObj = [{ 'test': 'none', 'status': 'error', 'message': 'no records found.', 'part': 'Unknown'}];
-                    response.write(JSON.stringify(MyJSONObj));
-                    response.end();
-                } else {
-                    for (var i = 0; i < rows.length; i++) {
-                        for (var x in rows[i]) {
-                            MyJSONObj[i] = rows[i];
-                        }
-                    }
-                    if (MyJSONObj.length == 0) { MyJSONObj[0] = { 'test': '', 'status': 'error', 'message': 'none', 'part': 'Unknown' }; };
-                    response.write(JSON.stringify(MyJSONObj));
-                    response.end();
-                    return;
-                }
-            }
-            else {
-                MyJSONObj[0] = { 'test': err, 'status': 'error', 'message': 'no records found.' };
-                response.write(JSON.stringify(MyJSONObj).replace('null', '-'));
-                response.end;
-                return;
-            }
-        });
-    });
+	response.writeHead(200, "OK", { 'Content-Type': 'application/json' });
+            
+	switch(ary["table"])
+	{
+	    case "certprint":
+	        var qry = fs.readFileSync('./HeatSheet/CodeBehind/CertQry.sql');
+	        qry = qry.toString().replace('@HEAT', Sanitize(ary["heat"]));
+	        qry = qry.toString().replace('@PART', Sanitize(ary["part"]));
+	        RunIt(request, response, fullBody, qry);
+	        break;
+	    default:
+	        response.write('[{"test":"","status":"error","message":"Invalid request."}]');
+	        response.end();
+            break;
+	}
 
 } // end rpt function
+
+    
+    function Sanitize(str){
+        if (str) { str = str.replace("'", "''").replace('"','""'); }else{str='';};
+        return str;
+    }
+
+    function RunIt(request, response, fullBody, qry) {
+        var MyJSONObj = new Array();
+        db.serialize(function () {
+
+            db.all(qry, function (err, rows) {
+                if (err) {
+                    MyJSONObj[0] = { 'test': err, 'status': 'error', 'message': 'Error: ' + err };
+                    response.write(JSON.stringify(MyJSONObj));
+                    response.end;
+                    return;
+                }
+                if (rows) {
+                    if (rows.length == 0) {
+                        MyJSONObj = [{ 'test': 'none', 'status': 'error', 'message': 'no records found.', 'part': 'Unknown' }];
+                        response.write(JSON.stringify(MyJSONObj));
+                        response.end();
+                    } else {
+                        for (var i = 0; i < rows.length; i++) {
+                            for (var x in rows[i]) {
+                                MyJSONObj[i] = rows[i];
+                            }
+                        }
+                        if (MyJSONObj.length == 0) { MyJSONObj[0] = { 'test': '', 'status': 'error', 'message': 'none', 'part': 'Unknown' }; };
+                        response.write(JSON.stringify(MyJSONObj));
+                        response.end();
+                        return;
+                    }
+                }
+                else {
+                    MyJSONObj[0] = { 'test': err, 'status': 'error', 'message': 'no records found.' };
+                    response.write(JSON.stringify(MyJSONObj).replace('null', '-'));
+                    response.end;
+                    return;
+                }
+            });
+        });
+    }
+
+
 
 exports.rpt = rpt
