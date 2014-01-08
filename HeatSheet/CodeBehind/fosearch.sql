a9c91ef6fbfbfa6b923c48102f7b1415281b3f41
johnh <johnatjencast@hotmail.com>
2014-01-08 11:32:32 -0600

    Added functionality for the FO Print so that we can get a list and print it off.
    Also included with this is some fixes for invalid requests for a jquery module and some png files that I didn't include with this.

--- HeatSheet/CodeBehind/fosearch.sql
+++ HeatSheet/CodeBehind/fosearch.sql
@@ -4,6 +4,8 @@ select
     , Part_No
     , Description
     , Customer
+    , descr_work
+    , case when finish_date is null or finish_date = '' then 'O' else 'C' end "Opened"
     , '' test
     , 'success' status
     , '' message
